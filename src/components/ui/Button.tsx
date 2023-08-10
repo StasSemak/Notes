6d075cfc639ae1e982a8ff6367156149ab075cb7
StasSemak <semaks051@gmail.com>
2023-08-10 15:13:30 +0300

    fix: button transition all

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -4,7 +4,7 @@ import { Loader2 } from "lucide-react"
 import React from "react"
 
 const buttonVariants = cva(
-    'inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors disabled:opacity-50 disabled:pointer-events-none',
+    'inline-flex items-center justify-center rounded-md text-sm font-medium transition-all disabled:opacity-50 disabled:pointer-events-none',
     {
         variants: {
             variant: {
